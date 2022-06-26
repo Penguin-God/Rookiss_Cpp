1d4bf9407becd15011ac6e1ed734b0c035a382d3
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-06-27 00:06:23 +0900

    OT ~ 변수와 레지스터

---
+++ Assembly Language/Hello Assembler.asm
@@ -0,0 +1,47 @@
+%include "io64.inc"
+
+section .text
+global CMAIN
+CMAIN:
+    mov rbp, rsp; for correct debugging
+    ;write your code here
+    
+    ;PRINT_STRING msg : Hello Wotld 출력
+    
+    ; 오른쪽 값을 왼쪽의 레지스터에 복사
+    ; mov reg1, 리터럴
+    ; mov reg1, reg2 
+
+    ; 레지스터 <-> 메모리
+    mov rax, a ; a 변수의 주솟값을 넣음
+    mov rax, [a] ; a 변수의 값을 a의 주소부터 8바이트만끔 쭉 뽑아서 다 쳐넣음 ㅋㅋ
+    mov al, [a] ; 마지막 부분만 바꾸기
+    
+    mov rax, 0
+    mov al, [a] ; 0으로 밀고 넣으면 rax를 a로 밀어버릴 수 있음
+    
+    mov [a], byte 0x55
+    mov [a], word 0x5566 ; 크기 초과하면 다음 메모리까지 침범해서 바꿔버림. 변수간의 독립성? 그딴거 없음
+    mov [a], cl
+    
+    xor rax, rax
+    ret
+    
+    ; 초기화 된 데이터
+    ; 변수이름, 크기, 초기값
+    ; 크기 종류 : db(1), dw(2), dd(4), dq(8)
+section .data
+    msg db "Hello Assembler", 0x00
+    a db 0x11
+    b dw 0x2222
+    c dd 0x33333333
+    d dq 0x4444444444444444
+
+
+    ; 초기화되지 않은 데이터    
+    ; 변수이름, 크기, 개수
+    ; 크기 종류 : resb(1), resw(2), resd(4), resq(8)
+section .bss
+    e resb 10
+
+    
